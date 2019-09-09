d39a37c455ffe98bd9157abb2a61ace64f2a75f7
Xander Horn <xander.horn@klarna.com>
2019-09-09 14:26:05 +0200

    one variable

--- R/map_categorical_encoding.R
+++ R/map_categorical_encoding.R
@@ -32,7 +32,7 @@ map.categorical.encoding <- function(data, x, y = NULL, max.levels = 10, min.per
   set.seed(seed)
   data <- as.data.frame(data[,c(x,y)])
   if(length(x) == 1){
-    names(data) <- x
+    names(data) <- c(x,y)
   }
 
   if(class(data[,y]) %in% c("factor","character")){
